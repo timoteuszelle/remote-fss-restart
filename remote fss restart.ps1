8cacc67ac6284758ed05307ee17a559902d66168
Tim Zelle <tim.zelle@sublie.me>
2021-02-17 14:16:18 +0100

    testingphase

--- remote fss restart.ps1
+++ remote fss restart.ps1
@@ -2,10 +2,13 @@
 Author: Tim Zelle
 Safe restart of Fss Launcher for FEWS.
 #> 
+
 $Computer = (Read-Host)
-Invoke-Command -ComputerName $Computer -ScriptBlock {
+$remotesession = New-PSSession -ComputerName $computer 
+Invoke-Command -Session $remotesession -ScriptBlock {
     Get-Service -Name FssLauncher | Stop-Service
     Get-Process -name fssLauncherService | Stop-Process -Force
     Get-ChildItem -Path "D:\fews\fss" -Recurse | Where-Object {$_.Name -like "*Running*", "*log*","awake"} | Remove-Item -Force
     Get-Service -Name FssLauncher | Start-Service
 }
+Remove-PSSession $remotesession
