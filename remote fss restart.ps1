aede8c278dbe8779fc05741df0246f7af7d93808
Tim Zelle <tim.zelle@sublie.me>
2021-02-17 14:52:37 +0100

    tested

--- remote fss restart.ps1
+++ remote fss restart.ps1
@@ -2,13 +2,12 @@
 Author: Tim Zelle
 Safe restart of Fss Launcher for FEWS.
 #> 
-
 $Computer = (Read-Host)
 $remotesession = New-PSSession -ComputerName $computer 
 Invoke-Command -Session $remotesession -ScriptBlock {
-    Get-Service -Name FssLauncher | Stop-Service
-    Get-Process -name fssLauncherService | Stop-Process -Force
-    Get-ChildItem -Path "D:\fews\fss" -Recurse | Where-Object {$_.Name -like "*Running*", "*log*","awake"} | Remove-Item -Force
-    Get-Service -Name FssLauncher | Start-Service
+    Get-Service -Name "FssLauncher" | Stop-Service
+    Get-Process -ProcessName "fssLauncherService" | Stop-Process -Force
+    Get-ChildItem -Path "D:\fews\fss" -Recurse | Where-Object {($_.Name -like "*Running*") -or ($_.Name -like "*log*") -or ($_.name -like "*awake*")} | Remove-Item -Force
+    Get-Service -Name "FssLauncher" | Start-Service
 }
 Remove-PSSession $remotesession
